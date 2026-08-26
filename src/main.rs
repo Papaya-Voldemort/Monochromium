7bf49588e9c3b8329fadf43021d7ad60463c5204
Eli Nelson <eli.n992@stu.nebo.edu>
2026-08-25 18:27:41 -0600

    Update main.rs with definitions for the Add and Check In functions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
-use clap::{Arg, Parser, Subcommand};
+use chrono::{NaiveDate, NaiveTime};
+use clap::{Arg, Parser, Subcommand, ValueEnum};
 
 #[derive(Parser)]
-#[clap( author, version, about)]
+#[clap(author, version, about)]
 struct MonoCLI {
     #[command(subcommand)]
     command: MonoCommands,
 
     #[clap(short, long, global = true)]
     copy: bool,
+
+    #[clap(short, long)]
+    animate: bool,
 }
 
 #[derive(Subcommand)]
 enum MonoCommands {
+    Add {
+        #[clap(short = 't', long = "type")]
+        note_type: Option<NoteTypes>,
 
+        #[clap(long)]
+        time: Option<NaiveTime>,
+
+        #[clap(long)]
+        date: Option<NaiveDate>,
+
+        #[clap(short, long)]
+        paste: bool,
+    },
+
+    CheckIn {
+        #[clap(long)]
+        time: Option<NaiveTime>,
+
+        #[clap(long)]
+        date: Option<NaiveDate>,
+
+        #[clap(short, long)]
+        paste: bool,
+    },
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum NoteTypes {
+    Idea,
+    CheckIn,
+    Random,
 }
 
 fn main() {
