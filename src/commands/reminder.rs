47bf7cebbe01167b31aebd306ee56fe57174957a
Eli Nelson <eli.n992@stu.nebo.edu>
2026-09-23 15:34:31 -0600

    add todo list on reminder.rs

--- src/commands/reminder.rs
+++ src/commands/reminder.rs
@@ -7,26 +7,35 @@ pub fn reminder(db: &Database, config: Config) -> Result<String, MonoError> {
     let current_datetime = Local::now().naive_local();
     let interval = config.checkin_interval_minutes;
 
+    let default = [
+        "-- monochromium --",
+        "No check-ins yet.",
+        "Start one with `mono checkin \"text\"`.",
+    ]
+    .join("\n");
+
+    let final_out: String = if config.show_todo_list {
+        let notes = db.read_rows(-1, Some(NoteTypes::Todo))?;
+        let mut pre: Vec<String> = vec![default, "".to_string(), "-- todo --".to_string()];
+        for note in notes {
+            pre.push(format!("[{}] {}", note.id, note.content));
+        }
+
+        pre.join("\n")
+    } else {
+        default
+    };
+
     let rows = db.search_rows(None, 1, Some(NoteTypes::CheckIn))?;
     if rows.is_empty() {
-        return Ok([
-            "-- monochromium --",
-            "No check-ins yet.",
-            "Start one with `mono checkin \"text\"`.",
-        ]
-        .join("\n"));
+        return Ok(final_out);
     }
 
     let past_datetime = rows[0].date;
     let time_passed: Duration = current_datetime - past_datetime;
 
     if time_passed.num_minutes() >= interval {
-        Ok([
-            "-- monochromium --",
-            "It's been a while since your last check-in.",
-            "Write one with `mono checkin \"text\"`.",
-        ]
-        .join("\n"))
+        Ok(final_out)
     } else {
         Ok(String::new())
     }
